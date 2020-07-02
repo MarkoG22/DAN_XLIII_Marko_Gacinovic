8a76c47ffdbb592274bb995e454a2730d15ec373
MarkoG22 <marko.gacinovic.el@gmail.com>
2020-07-02 18:16:27 +0200

    Admin View

---
+++ DAN_XLIII_Marko_Gacinovic/WPFEmployee/ViewModel/AdminViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFEmployee.View;
+
+namespace WPFEmployee.ViewModel
+{
+    class AdminViewModel : ViewModelBase
+    {
+        Admin admin;
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+    }
+}
